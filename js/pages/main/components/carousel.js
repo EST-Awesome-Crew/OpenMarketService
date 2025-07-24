9b49e8fdf204a5d52b1662276b5af227bbdbc749
MinKyeonghyeon <28599@naver.com>
2025-07-24 10:32:53 +0900

    refactor/ images 폴더의 이미지 소스를 가져와서 렌더하는 함수로 개선

--- js/pages/main/components/carousel.js
+++ js/pages/main/components/carousel.js
@@ -4,30 +4,38 @@ export function carouselControl() {
   const carouselButton2 = document.querySelector(".carousel-container_button2");
   const carouselSlide = document.querySelector(".carousel-slide");
   let count = 0;
-
+  const carouselImage = [
+    "../../../assets/images/blankit.png",
+    "../../../assets/images/keyling.jpg",
+    "../../../assets/images/keyling2.jpg",
+    "../../../assets/images/pouch.jpg",
+    "../../../assets/images/sticker.jpg",
+  ];
   /**
    * @param button
    * 캐러셀의 버튼을 누르면 캐러셀을 100%씩 이동시키는 함수
    */
   function carouselButton(button) {
+    const imageLength = carouselImage.length - 1;
     if (button === carouselButton1) {
       count--;
-      if (count === -1) count = 2;
+      if (count === -1) count = imageLength;
     } else {
       count++;
-      if (count > 2) count = 0;
+      if (count > imageLength) count = 0;
     }
     carouselSlide.style.transform = `translateX(-${count * 100}%)`;
   }
 
   //캐러셀의 이미지를 만드는 함수
-  carouselSlide.innerHTML = `
-    <li>
-    <img/>
 
-    </li>
-    `;
+  function renderCarouselImages() {
+    carouselSlide.innerHTML = carouselImage
+      .map(v => `<li class="carousel-image"><img src="${v}" alt=""></li>`)
+      .join("");
+  }
 
+  renderCarouselImages();
   carouselButton1?.addEventListener("click", () => {
     carouselButton(carouselButton1);
   });
