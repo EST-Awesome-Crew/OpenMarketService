a099b62aabe73e08df4414aae8fd768678fece93
sc.lee <sclee9040@gmail.com>
2025-07-25 11:09:14 +0900

    feat(mypageDropbox) : 마이페이지 드롭박스 구현, 로그아웃 구현

--- js/components/header.js
+++ js/components/header.js
@@ -1,27 +1,73 @@
-//헤더를 위한 js파일입니다.
 import { getAccessToken } from "../pages/login/auth.js";
-//로그인 유무에따른 마이페이지 ,로그인 텍스트 변경
-const $headerMenuText = document.querySelector(".header__menu-text");
-const $headerMenuLink = document.querySelector(".header__menu-link");
-const token = getAccessToken();
-if (token) {
-  $headerMenuText.innerHtml = "마이페이지";
-  $headerMenuLink.setAttribute("href", "/pages/mypage.html");
-} else {
-  $headerMenuText.innerHtml = "로그인";
-  $headerMenuLink.setAttribute("href", "/pages/login.html");
-}
-const $headerSearchInput = document.querySelector(".header__search-input");
-$searchForm.addEventListener("submit", e => {
-  e.preventDefault();
-  const input = $headerSearchInput.value.trim();
-  if (input === "") {
-    alert("검색어를 작성 해주세요");
-    $headerSearchInput.focus();
-    return;
+export function initHeader() {
+  //헤더를 위한 js파일입니다.
+  //로그인 유무에따른 마이페이지 ,로그인 텍스트 변경
+  const $headerMenuLogin = document.querySelector(".header__menu-login");
+  const $headerDropdown = document.querySelector(".dropdown");
+  const $searchForm = document.querySelector(".header__search-form");
+  const token = getAccessToken();
+  if (token) {
+    $headerDropdown.style.display = "block";
+  } else {
+    $headerMenuLogin.parentElement.parentElement.style.display = "block";
   }
-  const params = {
-    query: encodeURIComponent(input),
-  };
-  window.location.href = `/pages/main.html?${params.toString()}`;
+  const $headerSearchInput = document.querySelector(".header__search-input");
+  $searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const input = $headerSearchInput.value.trim();
+    if (input === "") {
+      alert("검색어를 작성 해주세요");
+      $headerSearchInput.focus();
+      return;
+    }
+    const params = {
+      query: encodeURIComponent(input),
+    };
+    window.location.href = `/pages/main.html?${params.toString()}`;
+  });
+
+  //마이페이지 클릭 토글
+  const $dropdown = document.querySelector(".dropdown");
+  const $headerMyText = document.querySelectorAll(".header__menu-link");
+  const $button = document.querySelector(".dropdown__button");
+  $button.addEventListener("click", e => {
+    e.stopPropagation();
+    $dropdown.classList.toggle("open");
+    if ($dropdown.classList.contains("open")) {
+      $headerMyText.forEach(menu => {
+        menu.style.color = "#21bf48"; // 글자색
+      });
+      const $cartImg = document
+        .querySelector(".header__menu-cart")
+        .parentElement.querySelector("img");
+      $cartImg.src = "../../assets/icons/icon-shopping-cart-2.svg ";
+      const $myPageImg = document.querySelector(".dropdown__button img");
+      $myPageImg.src = "../../assets/icons/icon-user-2.svg ";
+    } else {
+      $headerMyText.forEach(menu => {
+        menu.style.color = "#767676"; // 글자색
+      });
+      const $cartImg = document
+        .querySelector(".header__menu-cart")
+        .parentElement.querySelector("img");
+      $cartImg.src = "../../assets/icons/icon-shopping-cart.svg ";
+      const $myPageImg = document.querySelector(".dropdown__button img");
+      $myPageImg.src = "../../assets/icons/icon-user.svg ";
+    }
+  });
+
+  // 다른 영역 클릭 시 닫기
+  document.addEventListener("click", e => {
+    if (!$dropdown.contains(e.target)) {
+      $dropdown.classList.remove("open");
+    }
+  });
+}
+//로그아웃
+const $logoutBtn = document.querySelector(".dropdown__logout");
+$logoutBtn.addEventListener("click", e => {
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
+  localStorage.removeItem("user");
+  window.location.reload(true);
 });
